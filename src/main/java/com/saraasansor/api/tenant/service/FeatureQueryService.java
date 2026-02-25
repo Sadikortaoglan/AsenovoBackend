d830c6777c7cb3fd90c5d3cbc6292b286eb6566b
System Administrator <root@192.168.1.7>
2026-02-25 19:03:16 +0300

    hybrid 1-5 & fixesa

---
+++ src/main/java/com/saraasansor/api/tenant/service/FeatureQueryService.java
@@ -0,0 +1,48 @@
+package com.saraasansor.api.tenant.service;
+
+import com.saraasansor.api.exception.FeatureNotAvailableException;
+import com.saraasansor.api.tenant.TenantContext;
+import com.saraasansor.api.tenant.model.Feature;
+import com.saraasansor.api.tenant.model.Tenant;
+import com.saraasansor.api.tenant.model.TenantFeatureOverride;
+import com.saraasansor.api.tenant.repository.FeatureRepository;
+import com.saraasansor.api.tenant.repository.TenantFeatureOverrideRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class FeatureService {
+
+    private final TenantFeatureOverrideRepository repository;
+
+    @Lazy
+    @Autowired
+    private FeatureService self;
+
+    public FeatureService(TenantFeatureOverrideRepository repository) {
+        this.repository = repository;
+    }
+
+    @Cacheable(value = "tenantFeatures", key = "#tenantId")
+    public Set<String> getEnabledFeatures(Long tenantId) {
+        List<TenantFeatureOverride> tenantFeatureOverrideList = repository.findTenantFeatureOverridesByTenant_IdAndEnabledTrue(tenantId);
+        if(tenantFeatureOverrideList == null || tenantFeatureOverrideList.isEmpty()) return Collections.emptySet();
+
+        List<Feature> features = tenantFeatureOverrideList.stream().map(TenantFeatureOverride::getFeature).toList();
+        return features.stream().map(Feature::getCode).collect(Collectors.toSet());
+    }
+
+    public void assertEnabled(String featureCode) throws FeatureNotAvailableException {
+        Long tenant = TenantContext.getTenantId();
+        if (!self.getEnabledFeatures(tenant).contains(featureCode)) {
+            throw new FeatureNotAvailableException("Feature '" + featureCode + "' is not enabled for tenant '" + tenant + "'.");
+        }
+    }
+}
